013c3a165b7df5c4a01fe871c4e8a037262b346d
Hoang <luonghoanganh9801@gmail.com>
2020-12-09 21:12:32 +0100

    updated follow and unfollow views

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE, related_name="post", null=True)
+    user = models.CharField(max_length=64)
+    content = models.CharField(max_length=64)
+    likes = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
+    
+
+
+
+class Like(models.Model):
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE, related_name="liked", null=True)
+    post_id = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="like", null=True)
+    
+
+class UserFollowing(models.Model):
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE, related_name="following", null=True)
+    following_user_id = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers", null=True)
+    time = models.DateTimeField(auto_now_add=True)
--- project4/network/views.py
+++ project4/network/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from .models import User
-
+from .models import User, Post, Like, UserFollowing
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 def index(request):
-    return render(request, "network/index.html")
+    try:
+        posts = Post.objects.all().order_by('-time')
+    except:
+        posts = None
+    return render(request, "network/index.html", {"posts": posts})
 
 
 def login_view(request):
@@ -61,3 +66,110 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+    
+    
+    
+@login_required('index')
+def post_submit(request):
+    if request.method == "POST":
+        content = request.POST['content']
+        post = Post.objects.create(user_id=request.user.id, user=request.user.username, content=content, likes=0)
+        post.save()
+        return render(request, "network/index.html")
+    else:
+        return redirect("index")
+
+def all_posts(request):
+    posts = Post.objects.all().order_by('-time')
+    paginator = Paginator(posts, 10)
+    if request.GET.get("page") != None:
+        try:
+            posts = paginator.page(request.GET.get("page"))
+        except:
+            posts = paginator.page(1)
+    else:
+        posts = paginator.page(1)
+    return render(request, "network/allposts.html", {
+            "posts": posts
+            })      
+
+@login_required()
+def profile(request, profile_id):
+    try:
+        user = User.objects.get(id=request.user.id)
+        profile = User.objects.get(id=profile_id)
+        try:
+            following = profile.following.all()
+            followers = profile.followers.all()
+            if user in followers:
+                followed = True
+            else:
+                followed = False
+        except:
+            following = None
+            followers = None
+            followed = False
+        posts = Post.objects.filter(user_id=profile_id).order_by('-time') 
+        paginator = Paginator(posts, 10)
+        
+        try:
+            posts = paginator.page(request.GET.get("page"))
+        except:
+            posts = paginator.page(1)
+        
+        
+        return render(request, 'network/profile.html', {
+            'user': user,
+            'posts': posts,
+            'following': following,
+            'followers': followers,
+            'followed': followed
+            })
+        
+    except:
+        return render(request, "network/profile.html", {
+            "error": True
+            })
+    
+@login_required
+def follow(request, follow_id):
+    following = UserFollowing.objects.create(user_id=follow_id, follow_user_id=request.user.id)
+    follower = UserFollowing.objects.create(user_id=request.user.id, follow_user_id=follow_id)
+    following.save()
+    follower.save()
+   
+    return redirect("profile", profile_id=follow_id)
+
+
+
+@login_required
+def unfollow(request, unfollow_id):
+    following = UserFollowing.objects.create(user_id=unfollow_id, follow_user_id=request.user.id)
+    follower = UserFollowing.objects.create(user_id=request.user.id, follow_user_id=unfollow_id)
+    following.remove()
+    follower.remove()
+    
+    return redirect("profile", profile_id=unfollow_id)
+    
+  
+@login_required  
+def following(request):
+    user = User.objects.get(id=request.user.id)
+    following = user.following.all()
+    posts = Post.objects.filter(user_id=following).order_by('-time')
+    paginator = Paginator(posts, 10)
+    if request.GET.get("page") != None:
+        try:
+            posts = paginator.page(request.GET.get("page"))
+        except:
+            posts = paginator.page(1)
+    else:
+        posts = paginator.page(1)
+    return render(request, "network/allposts.html", {
+            "posts": posts
+            }) 
+        
+        
+
+
+    
